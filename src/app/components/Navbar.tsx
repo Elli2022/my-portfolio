6dac8c08f57a407fcd414faf8e8868963de12a13
Eleonora Nocentini Sköldebrink <eleonora.nocentini@gmail.com>
2024-01-07 20:43:35 +0100

    hamburgermenu + gradient background

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,12 +1,31 @@
-// src/components/Navbar/Navbar.js
-import React from "react";
+// src/components/Navbar/Navbar.tsx
+
+"use client";
+import React, { useState } from "react";
 import Link from "next/link";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    console.log("Current isOpen value (before toggle):", isOpen);
+    setIsOpen((current) => {
+      console.log("New isOpen value (after toggle):", !current);
+      return !current;
+    });
+  };
+
   return (
     <nav>
-      <div className="navbar-container">
-        <Link href="/">Home</Link>
+      <div className="menu-icon" onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div className={`navbar-container ${isOpen ? "active" : ""}`}>
+        <Link href="/" onClick={() => setIsOpen(false)}>
+          Home
+        </Link>
         <Link href="/about">About</Link>
         <Link href="/projects">Projects</Link>
         <Link href="/contact">Contact</Link>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,33 +5,35 @@ import Navbar from "./components/Navbar";
 function Home() {
   return (
     <>
-      <Navbar />
-      <div className="home">
-        <h1>Välkommen till Min Portfölj</h1>
-        <p>
-          Hej, jag är Eleonora – en mångsidig front-end utvecklare med djupa
-          rötter i säkerhetsarbete och en passion för teknik och programmering.
-          Som en livslång lärande har jag nyligen omskolat mig från en karriär
-          inom Kriminalvården, där jag finslipade min förmåga att tänka kritiskt
-          och hantera kritiska situationer, till att nu följa min passion inom
-          webbutveckling.
-        </p>
-        <p>
-          Min tekniska resa är stärkt av en solid utbildningsgrund med fokus på
-          JavaScript, moderna webbteknologier och agil metodik. Jag kombinerar
-          detta med en kandidatexamen i Service Management och erfarenheter som
-          sträcker sig från cybersäkerhet till projektledning, vilket ger mig en
-          unik förmåga att förstå både tekniska och affärsmässiga aspekter av
-          projekt jag engagerar mig i.
-        </p>
-        <p>
-          Utöver teknik, brinner jag för att skapa säkra och tillgängliga
-          digitala miljöer som berikar användarens upplevelse. Jag är en
-          lagspelare, redo att ta mig an nya utmaningar och bidra med min
-          mångfaldiga erfarenhet till ditt nästa projekt.
-        </p>
+      <div className="min-h-screen bg-gradient-to-r from-orange-500 to-purple-500">
+        <Navbar />
+        <div className="home">
+          <h1>Välkommen till Min Portfölj</h1>
+          <p>
+            Hej, jag är Eleonora – en mångsidig front-end utvecklare med djupa
+            rötter i säkerhetsarbete och en passion för teknik och
+            programmering. Som en livslång lärande har jag nyligen omskolat mig
+            från en karriär inom Kriminalvården, där jag finslipade min förmåga
+            att tänka kritiskt och hantera kritiska situationer, till att nu
+            följa min passion inom webbutveckling.
+          </p>
+          <p>
+            Min tekniska resa är stärkt av en solid utbildningsgrund med fokus
+            på JavaScript, moderna webbteknologier och agil metodik. Jag
+            kombinerar detta med en kandidatexamen i Service Management och
+            erfarenheter som sträcker sig från cybersäkerhet till
+            projektledning, vilket ger mig en unik förmåga att förstå både
+            tekniska och affärsmässiga aspekter av projekt jag engagerar mig i.
+          </p>
+          <p>
+            Utöver teknik, brinner jag för att skapa säkra och tillgängliga
+            digitala miljöer som berikar användarens upplevelse. Jag är en
+            lagspelare, redo att ta mig an nya utmaningar och bidra med min
+            mångfaldiga erfarenhet till ditt nästa projekt.
+          </p>
+        </div>
+        <Footer />
       </div>
-      <Footer />
     </>
   );
 }
